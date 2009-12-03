e5b68a68fb3e4718b74f91096e8828b5176a1cec
Phillou <philippemeyer@codd.u-strasbg.fr>
2009-12-03 15:00:12 +0100

    2eme semaine 2

---
+++ app/controllers/application.rb
@@ -0,0 +1,16 @@
+# app/controllers/application.rb
+class ApplicationController < ActionController::Base
+  filter_parameter_logging :password, :password_confirmation
+  helper_method :current_user_session, :current_user
+
+  private
+    def current_user_session
+      return @current_user_session if defined?(@current_user_session)
+      @current_user_session = UserSession.find
+    end
+
+    def current_user
+      return @current_user if defined?(@current_user)
+      @current_user = current_user_session && current_user_session.user
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,9 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :user_session
   map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
 
+  map.resource :account, :controller => "users"
+  map.resources :users
+
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
   
